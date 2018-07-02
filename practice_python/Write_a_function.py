49fbea90eb714b26528cf0d5838d9bac74e328c6
epicarts <0505zxc@gmail.com>
2018-07-02 23:39:56 +0900

    upload file

---
+++ practice_python/Write_a_function.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+#python3
+#Gregorian clendar 윤년계산
+
+
+year = int(input())
+print(is_leap(year))
+
+def is_leap(year):
+    leap = False
+
+    # Write your logic here
+    if year % 4 is 0:
+        leap = True
+        if year % 100 is 0:
+            leap = False
+            if year % 400 is 0:
+                leap = True
+
+    return leap
