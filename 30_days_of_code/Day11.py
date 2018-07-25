4ba2c4888bf35b45517c1a265938d7b35de6ce89
epicarts <0505zxc@gmail.com>
2018-07-25 20:54:05 +0900

    update code

--- 30_days_of_code/Day11.py
+++ 30_days_of_code/Day11.py
@@ -6,18 +6,9 @@ import random
 import re
 import sys
 
-'''
-[[1, 1, 1, 0, 0, 0],
- [0, 1, 0, 0, 0, 0],
- [1, 1, 1, 0, 0, 0],
- [0, 0, 2, 4, 4, 0],
- [0, 0, 0, 2, 0, 0],
- [0, 0, 1, 2, 4, 0]]
-
-'''
-
 
 if __name__ == '__main__':
+    '''
     arr = [[1, 1, 1, 0, 0, 0],
      [0, 1, 0, 0, 0, 0],
      [1, 1, 1, 0, 0, 0],
@@ -31,25 +22,23 @@ if __name__ == '__main__':
 
     print(arr[0][0],arr[1][0],arr[2][0],arr[3][0])#column
 
-    sum_list =[]
     a = []
-    b = [_ for _ in range(6)]
-    print(b)
 
-    for _ in range(6):
-        #arr.append(list(map(int, input().rstrip().split())))
-    print(arr)
-    maximum = []
     type(arr[0][0:3])
     type(sum(arr[0][0:3]))
 
     sum(arr[0][0:3]) + sum(arr[1][1]) + sum(arr[2][0:3])
+    '''
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
 
+    sum_list = []
 
     for x in range(4):
         for y in range(4):
-            arr_sum = sum(arr[x][y:y + 3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
-            maximum.append(str(arr_sum))
+            arr_sum = sum(arr[x][y:y+3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
+            sum_list.append(str(arr_sum))
 
 
-    print(maximum)
+    print(max(map(int,sum_list)))#str => int => maxvalue
